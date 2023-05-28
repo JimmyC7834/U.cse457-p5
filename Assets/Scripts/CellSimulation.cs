ee54db0467c14208aa0f5470fcf188e397a92b13
Fettuchoony <106009124+Fettuchoony@users.noreply.github.com>
2023-05-27 17:02:17 -0700

    Equidistant Sphere creation
    Sphere now spawns made of equidistant particles instead of weird clumped up ones. Just gotta digure out the joints, prob gonna use a sphere checker for proximity.

--- Assets/Scripts/CellSimulation.cs
+++ Assets/Scripts/CellSimulation.cs
@@ -7,6 +7,7 @@ public class CellSimulation : MonoBehaviour
 
     [SerializeField] private bool _showDebug;
     [SerializeField] private int _radius;
+    [SerializeField] private int _totalPoints;
     [SerializeField] private float _spacing;
     private PhysicsObject[] _lipids;
     private SoftBodySimulation _softBodySimulation;
@@ -22,7 +23,7 @@ public class CellSimulation : MonoBehaviour
     private void Start()
     {
         InitSphere();
-        _softBodySimulation.Enabled = false;
+        _softBodySimulation.Enabled = true;
         _softBodySimulation.ShowDebug = _showDebug;
     }
 
@@ -43,71 +44,119 @@ public class CellSimulation : MonoBehaviour
         int r = _radius;
         // add nodes
         // Vector3 offset = new Vector3(-_spacing, 0, -_spacing);
-        Vector3 baseV = new Vector3(r, r, r);
-        for (int i = 0; i < r; i++)
-        {
-            for (int j = 0; j < r; j++)
-            {
-                for (int k = 0; k < r; k++) {
-                    int index = (i * r * r) + (j * r) + k;
-                    Vector3 pos = new Vector3(
-                        i - r/2,
-                        j - r/2,
-                        k - r/2);
-                    pos.Normalize();
-                    pos.Scale(baseV);
-                    PhysicsObject node = CreateLipid(pos);
-                    _softBodySimulation.AddNode(node);
-                    _lipids[index] = node;
-                }
+        // Vector3 baseV = new Vector3(Mathf.Sqrt(r), Mathf.Sqrt(r), Mathf.Sqrt(r));
+        // for (int i = 0; i < r; i++)
+        // {
+        //     for (int j = 0; j < r; j++)
+        //     {
+        //         for (int k = 0; k < r; k++) {
+        //             int index = (i * r * r) + (j * r) + k;
+        //             Vector3 pos = new Vector3(
+        //                 (i - r/2),
+        //                 (j - r/2),
+        //                 (k - r/2));
+        //             pos.Normalize();
+        //             pos.Scale(baseV);
+        //             PhysicsObject node = CreateLipid(pos/(1/_spacing));
+        //             _softBodySimulation.AddNode(node);
+        //             _lipids[index] = node;
+        //         }
+        //     }
+        // }
+
+        // Expiremental Placement
+        int count = 0;
+        float alpha = (4*Mathf.PI*r*r)/_totalPoints;
+        float d = Mathf.Sqrt(alpha);
+        int M_v = (int) Mathf.Round(Mathf.PI/d);
+        float d_v = Mathf.PI / M_v;
+        float d_y = alpha / d_v;
+        for (int i = 0; i < M_v; i++) {
+            float v = Mathf.PI*(i + 0.5f) / M_v;
+            int M_y = (int) Mathf.Round(2*Mathf.PI*Mathf.Sin(v)/d_y);
+            for (int j = 0; j < M_y; j++) {
+                // int index = (i * r) + j;
+                float y = 2*Mathf.PI*j/M_y;
+                Vector3 pos = new Vector3(Mathf.Sin(v)*Mathf.Cos(y), Mathf.Sin(v)*Mathf.Sin(y), Mathf.Cos(v));
+                PhysicsObject node = CreateLipid(pos);
+                _softBodySimulation.AddNode(node);
+                _lipids[count] = node;
+                // for (int k = 0; k < _totalPoints; k++) {
+                //     float z = 2*k*(1/r) - r;
+                //     float y = k*(1/(2*Mathf.PI));
+                //     Vector3 pos = new Vector3(Mathf.Sqrt(r*r-z*z)*Mathf.Cos(y), Mathf.Sqrt(r*r-z*z)*Mathf.Sin(y), z);
+                //     PhysicsObject node = CreateLipid(pos);
+                //     _softBodySimulation.AddNode(node);
+                //     _lipids[index] = node;
+                // }
+                count++;
             }
         }
+
+
+
+
         
         // add bonds
-        for (int i = 0; i < r; i++)
-        {
-            for (int j = 0; j < r; j++)
-            {
-                for (int k = 0; k < r; k++) {
-                    int index = (i * i * r) + (j * r) + k;
-                    // add bonds
-                    if (j < r - 1)
-                        _softBodySimulation.AddJoint(CreateJoint(
-                                _lipids[index],
-                                _lipids[index + 1]
-                            ));
-                    
-                    if (i < r - 1)
-                        _softBodySimulation.AddJoint(CreateJoint(
-                            _lipids[index],
-                            _lipids[index]
-                        ));
+        // for (int i = 0; i < r; i++)
+        // {
+        //     for (int j = 0; j < r; j++)
+        //     {
+        //         int index = ;
+        //         // add bonds
+        //         if (j < r - 1)
+        //             _softBodySimulation.AddJoint(CreateJoint(
+        //                     _lipids[index],
+        //                     _lipids[index + 1]
+        //                 ));
+                
+        //         if (i < r - 1)
+        //             _softBodySimulation.AddJoint(CreateJoint(
+        //                 _lipids[index],
+        //                 _lipids[index]
+        //             ));
+                
+        //         if (j < r - 1 && i < r - 1)
+        //             _softBodySimulation.AddJoint(CreateJoint(
+        //                 _lipids[index],
+        //                 _lipids[index + 1]
+        //             ));
+                
+        //         if (j > 0 && i < r - 1)
+        //             _softBodySimulation.AddJoint(CreateJoint(
+        //                 _lipids[index],
+        //                 _lipids[index + r - 1]
+        //             ));             
+        //         }
+        // }
 
-                    if (k < r - 1)
-                        _softBodySimulation.AddJoint(CreateJoint(
-                            _lipids[index],
-                            _lipids[index + (j*r)]
-                        ));
-                    
-                    if (j < r - 1 && i < r - 1)
-                        _softBodySimulation.AddJoint(CreateJoint(
-                            _lipids[index],
-                            _lipids[index + 1]
-                        ));
-                    
-                    if (j > 0 && i < r - 1)
-                        _softBodySimulation.AddJoint(CreateJoint(
-                            _lipids[index],
-                            _lipids[index + r - 1]
-                        ));
-                    
-                    if (k > 0 && i < r - 1)
-                        _softBodySimulation.AddJoint(CreateJoint(
-                            _lipids[index],
-                            _lipids[index + r - 1]
-                        ));
-                }                
-            }
-        }
+
+        for (int index = 1; index < count; index++)
+        {
+            // add bonds
+            // if (index < count - 1)
+            //     _softBodySimulation.AddJoint(CreateJoint(
+            //             _lipids[index],
+            //             _lipids[index + 1]
+            //         ));
+            
+            // if (index < count - 1)
+            //     _softBodySimulation.AddJoint(CreateJoint(
+            //         _lipids[index],
+            //         _lipids[index - 1]
+            //     ));
+
+            // if (index < count - 1)
+            //     _softBodySimulation.AddJoint(CreateJoint(
+            //         _lipids[index],
+            //         _lipids[index + (count/2) % count]
+            //     ));
+
+            // if (index < count - 1)
+            //     _softBodySimulation.AddJoint(CreateJoint(
+            //         _lipids[index],
+            //         _lipids[index]
+            //     ));
+        }    
     }
 }
